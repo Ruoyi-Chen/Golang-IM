cc33093af8301fa6f745618f4d2396a550e93ca2
Ruoyi-Chen <chenruoyisz@gmail.com>
2023-11-18 22:10:12 +0800

    6. 超时强踢

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -61,7 +62,10 @@ func (this *Server) Handler(conn net.Conn) {
 	// 用户上线
 	user.Online()
 
-	// 3. 接收客户端发送的消息
+	// 监听用户是否活跃的channel
+	isLive := make(chan bool)
+
+	// 接收客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
 		for {
@@ -80,11 +84,31 @@ func (this *Server) Handler(conn net.Conn) {
 			msg := string(buf[:n-1])
 			// 将得到的消息进行广播
 			user.DoMessage(msg)
+
+			// 用户的任意消息代表当前用户是活跃的
+			isLive <- true
 		}
 	}()
 
 	// 当前handler阻塞
-	select {}
+	for {
+		select {
+		case <-isLive:
+			// 当前用户是活跃的，应该重置定时器
+			// 不做任何事情，就是为了激活select，激活定时器（就是这里没做，但是到下一个语句，会执行这个判断条件）
+		case <-time.After(time.Second * 10):
+			// 已经超时
+			// 将当前的User强制关闭
+
+			user.SendMsg("你被踢了")
+			// 销毁用户资源
+			close(user.C)
+			// 关闭资源
+			conn.Close()
+			// 退出当前handler
+			return
+		}
+	}
 }
 
 // Start 启动服务器的接口
