cac2b7784da26888870c9ae31c7e6decb4d7c0ef
Ruoyi-Chen <chenruoyisz@gmail.com>
2023-11-19 16:59:49 +0800

    8. 创建客户端，私聊模式

--- client.go
+++ client.go
@@ -73,6 +73,51 @@ func (client *Client) UpdateName() bool {
 	return true
 }
 
+// SelectUsers 查询在线用户
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn Write err: ", err)
+		return
+	}
+}
+
+// PrivateChat 私聊模式
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+
+	client.SelectUsers()
+	fmt.Println(">>>>> 请输入聊天对象[用户名]， exit退出")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println(">>>>> 请输入消息内容，exit退出")
+		fmt.Scanln(&chatMsg)
+
+		for chatMsg != "exit" {
+			// 消息不为空则发送
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn Write err: ", err)
+					break
+				}
+			}
+
+			chatMsg = ""
+			fmt.Println(">>>>> 请输入消息内容，exit退出")
+			fmt.Scanln(&chatMsg)
+		}
+
+		client.SelectUsers()
+		fmt.Println(">>>>> 请输入聊天对象[用户名]， exit退出")
+		fmt.Scanln(&remoteName)
+	}
+}
+
 func (client *Client) PublishChat() {
 	// 提示用户输入消息
 	var chatMsg string
@@ -111,7 +156,7 @@ func (client *Client) Run() {
 			break
 		case 2:
 			// 私聊模式
-			fmt.Println(">>>> 私聊模式")
+			client.PrivateChat()
 			break
 		case 3:
 			// 更新用户名
